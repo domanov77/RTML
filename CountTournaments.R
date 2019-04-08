db53b5b9321f2277843a8e491da2555ea4f6cd4a
domanov77 <domanov@gmail.com>
2019-04-08 11:17:33 +0200

    Initial commit

---
+++ CountTournaments.R
@@ -0,0 +1,199 @@
+source("DataFunctions.R")
+
+
+## system.time(dbsack <- PrepareDataSackmann(dir="SackmanGit/tennis_atp", pattern="atp_matches_[[:digit:]]{4}.csv"))
+## just quali/challenger
+system.time(dbsack <- PrepareDataSackman(dir="DataSackman", pattern="atp_matches_qual_chall_[[:digit:]]{4}.csv", excludeDavis=FALSE))
+ss <- dbsackc$all_data
+
+system.time(dbtop <- PrepareDataTop())
+tt <- dbtop$all_data
+
+cc <- rbind(tt, ss)
+
+
+
+roger_for_sack <- SearchByPlayer("Roger Federer", data=dbsack)
+roger_for_top  <- SearchByPlayer("Roger Federer", data=dbtop)
+
+ss <- dbsack$all_data
+tt[tourney_level=="A", .(tourney_name, year)]
+tt[tourney_level=="F", .(tourney_name, year)]
+
+
+tt[tourney_level=="G" & winner_name=="Roger Federer", .N, .(tourney_name, year)]
+tt[tourney_level=="G" & loser_name=="Roger Federer", .(tourney_name, year)]
+tt[tourney_level=="G" & round=="F", .(tourney_name, year, winner_name)]
+
+aa <- as.data.frame(tt[tourney_name %in% c("London", "Tour Finals"), .(tourney_name, year, tourney_level)])
+
+tt[tourney_name=="Roland Garros" & (winner_name=="Rafael Nadal" | loser_name == "Rafael Nadal"), .(year, loser_name, score)]
+
+scores <- tt[tourney_name=="Roland Garros" & (winner_name=="Rafael Nadal" | loser_name == "Rafael Nadal"), .(year, loser_name, score)]
+scores[sapply(strsplit(scores$score, " "), length)==5,]
+
+tt[tourney_name=="London", .(tourney_name, year, tourney_level)]
+
+## how many bagels did John Isner suffer?
+scores_win <- tt[winner_name=="John Isner", .(year, tourney_name, winner_name, loser_name, score)]
+w <- grep("0-6", scores_win$score, fixed=TRUE)
+scores_los <- tt[loser_name=="John Isner", .(year, tourney_name, winner_name, loser_name, score)]
+l <- grep("6-0", scores_los$score, fixed=TRUE)
+tbagel <- rbind(scores_win[w,], scores_los[l,])
+nrow(tbagel)
+
+## how many bagels did John Isner inflict?
+scores_win <- tt[winner_name=="John Isner", .(year, tourney_name, winner_name, loser_name, score)]
+w <- grep("6-0", scores_win$score, fixed=TRUE)
+scores_los <- tt[loser_name=="John Isner", .(year, tourney_name, winner_name, loser_name, score)]
+l <- grep("0-6", scores_los$score, fixed=TRUE)
+tbagel <- rbind(scores_win[w,], scores_los[l,])
+nrow(tbagel)
+
+## Finali in straight sets vinte nei 1000
+fm <- tt[tourney_level=="M" & round=="F", .(year, tourney_name, winner_name, loser_name, score, best_of)]
+fm <- fm[lengths(regmatches(fm$score, gregexpr("-", fm$score)))==(fm$best_of+1)/2,]
+table <- fm[year>1999, .(year, tourney_name, winner_name, loser_name, score, best_of)]
+library(gridExtra)
+cairo_pdf("table_straight_sets.pdf", height=40, width=10)
+grid.table(as.data.frame(table))
+dev.off()
+
+
+### Most recurrent score for Federer
+wins <- tt[winner_name=="Roger Federer", .N, by=.(score, surface)] 
+setorder(wins, -N)
+ 
+wins[N>5,]
+
+### Bagels Mc
+
+## how many bagels did Mackenzie McDonald suffer?
+scores_win <- tt[winner_name=="Mackenzie Mcdonald", .(year, tourney_name, surface, round, winner_name, loser_name, score)]
+w <- grep("0-6", scores_win$score, fixed=TRUE)
+scores_los <- tt[loser_name=="Mackenzie Mcdonald", .(year, tourney_name, surface, round, winner_name, loser_name, score)]
+l <- grep("6-0", scores_los$score, fixed=TRUE)
+tbagel <- rbind(scores_win[w,], scores_los[l,])
+nrow(tbagel)
+OutputTableToPng(tbagel, "MacBagelLos.png")
+## how many bagels did Mac inflict?
+scores_win <- tt[winner_name=="Mackenzie Mcdonald", .(year, tourney_name, surface, round, winner_name, loser_name, score)]
+w <- grep("6-0", scores_win$score, fixed=TRUE)
+scores_los <- tt[loser_name=="Mackenzie Mcdonald", .(year, tourney_name, surface, round, winner_name, loser_name, score)]
+l <- grep("0-6", scores_los$score, fixed=TRUE)
+tbagel <- rbind(scores_win[w,], scores_los[l,])
+nrow(tbagel)
+OutputTableToPng(tbagel, "MacBagelWn.png")
+ 
+ 
+
+fedal <- h2h("Roger Federer", "Rafael Nadal", tt)
+fedal[, .N, by=.(winner_name, surface)]
+fedal[, .N, by=.(winner_name)]
+
+roge <- PlayerMatches("Roger Federer", tt)
+rafa <- PlayerMatches("Rafael Nadal", tt)
+nole <- PlayerMatches("Novak Djokovic", tt)
+
+
+roge[grep("W/O|RET", roge$score),.N, by=.(winner_name)]
+rafa[grep("W/O|RET", rafa$score),.N, by=.(winner_name)]
+nole[grep("W/O|RET", nole$score),.N, by=.(winner_name)]
+
+ 
+ 
+ 
+grep("Tour Finals", tt$tourney_name)
+tt[grep("^Tour Finals", tt$tourney_name),]
+## search ALL tournaments played by ALL players in the database
+
+## parallel version, Linux; 7 cores, ~1.1 sec
+system.time(tot <- parallel::mclapply(dbsack$players, SearchByPlayer, data=dbsack, mc.cores=7))
+## serial version, ~11 sec
+system.time(tot2 <- lapply(dbsack$players, SearchByPlayer, data=dbsack))
+names(tot) <- names(tot2) <- dbsack$players
+all.equal(tot, tot2) ## TRUE
+
+## most total entries 
+te <- sapply(tot, function(x) sum(x$N))
+te <- sort(te, decreasing=TRUE)
+
+head(te)
+##  Fabrice Santoro   Feliciano Lopez   Mikhail Youzhny     Jimmy Connors      David Ferrer Fernando Verdasco 
+##              443               424               420               390               388               384 
+
+system.time(roger_top <- SearchByPlayer("Roger Federer", data=dbtop))
+## serial version, ~11 sec
+system.time(tot_top <- lapply(dbtop$players, SearchByPlayer, data=dbtop))
+names(tot_top) <- dbtop$players
+
+SearchByPlayer("Roger Federer", data=dbtop, tour="Tour Finals")
+tep <- sapply(tot_top, function(x) sum(x$N))
+tep <- sort(tep, decreasing=TRUE)
+
+head(tep)
+
+
+## punti ATP medi negli slam:Fe
+losses <- tt[tourney_level=="G" & (loser_name=="Roger Federer" ), ]
+wins <- tt[tourney_level=="G" & (winner_name=="Roger Federer" )&round=="F",  ]
+
+points_ <- c(R128=10, R64=45, R32=90, R16=180, QF=360, SF=720, F=1200, W=2000)
+##points_ <- c(R128=0, R64=1, R32=2, R16=3, QF=4, SF=5, F=6, W=7)
+
+losses[, points:=points_[match(losses$round , names(points_))] ]
+wins[,points:=points_[length(points_)] ]
+tots <- rbind(losses, wins)
+ tots[ , .(points=mean(points))] 
+res <- tots[ , .(points=mean(points), .N ), by=.(tourney_name)]
+setorder(res, -points)
+res
+OutputTableToPng(res, "average_points_slam_fed.png")
+
+res2 <- tots[ , .(points=mean(points), .N ), by=.(surface)]
+setorder(res2, -points)
+OutputTableToPng(res2, "average_points_slam_surf_fed.png")
+
+
+
+## punti ATP medi negli slam:Nadal
+losses <- tt[tourney_level=="G" & (loser_name=="Rafael Nadal" ), ]
+wins <- tt[tourney_level=="G" & (winner_name=="Rafael Nadal" )&round=="F",  ]
+
+## points <- c(R128=10, R64=45, R32=90, R16=180, QF=360, SF=720, F=1200, W=2000)
+
+ losses[, points:=points_[match(losses$round , names(points_))] ]
+wins[,points:=points_[length(points_)] ]
+tots <- rbind(losses, wins)
+ tots[ , .(points=mean(points))] 
+
+res <- tots[ , .(points=mean(points), .N ), by=.(tourney_name)]
+setorder(res, -points)
+OutputTableToPng(res, "average_points_slam_rafa.png")
+
+res2 <- tots[ , .(points=mean(points), .N ), by=.(surface)]
+setorder(res2, -points)
+OutputTableToPng(res2, "average_points_slam_surf_rafa.png")
+
+
+
+
+## punti ATP medi negli slam: Nole
+losses <- tt[tourney_level=="G" & (loser_name=="Novak Djokovic" ), ]
+wins <- tt[tourney_level=="G" & (winner_name=="Novak Djokovic" )&round=="F",  ]
+
+points <- c(R128=10, R64=45, R32=90, R16=180, QF=360, SF=720, F=1200, W=2000)
+
+points[match(losses$round , names(points))]
+losses[, points:=points[match(losses$round , names(points))] ]
+wins[,points:=2000]
+tots <- rbind(losses, wins)
+ tots[ , .(points=mean(points))] 
+
+res <- tots[ , .(points=mean(points), .N ), by=.(tourney_name)]
+setorder(res, -points)
+OutputTableToPng(res, "average_points_slam_nole.png")
+
+res2 <- tots[ , .(points=mean(points), .N ), by=.(surface)]
+setorder(res2, -points)
+OutputTableToPng(res2, "average_points_slam_surf_nole.png")
---
+++ DataFunctions.R
@@ -0,0 +1,113 @@
+### Functions to interrogate the databases of matches
+
+### Time-stamp: "Last modified 2019-04-05 19:58:54 delucia"
+library(data.table)
+
+PrepareDataTop <- function(file="DataTop/20190405-DataTop.csv") {
+    
+    ## read the data
+    read_timing <- system.time(data <- data.table::fread(file)) ## removed fill=TRUE since I corrected the database!
+    cat(paste(":: Reading done in", round(read_timing[3], 5), "s\n")) 
+    
+    ## exclude Davis cup matches and select only relevant columns
+    ind_davis <- grep("^Davis", data$tourney_name)
+    data <- data[-ind_davis, ] ## c("tourney_date", "tourney_name","tourney_level", "draw_size", "round", "winner_name","loser_name")]
+
+    ## Define a new "year" column
+    data$year <- substr(data$tourney_date, 1, 4)
+
+    ## find out all tournaments
+    all_tourn <- sort(unique(data$tourney_name))
+    ## find out all players
+    all_names <- sort(unique(c(data$winner_name, data$loser_name)))
+
+    ## Define a new "year" column
+    data$year <- substr(data$tourney_date, 1, 4)
+
+    ## here .id column is the year
+    ## group all the finals per tournament, per winner and per year
+    nw <- data[,.N, by=.(tourney_name, winner_name, year)]
+    ## this was faster but didn't work for round robin tournaments such as Tour Finals
+    ## nw <- data[round=="F",.N, by=.(tourney_name, winner_name, year)]
+
+    ## find all losers per tournament and per year
+    nl <- data[,.N, by=.(tourney_name, loser_name, year)]
+
+    ## perform the union of the two sets! Here, each row represents a player who played in that tournament in that year
+    dt <- rbindlist(list(nw, nl), use.names=FALSE)[, 1:3]
+    if (haskey(dt)) 
+        setkey(dt,NULL)
+    ## we need now to remove duplicates
+    dt <- unique(dt)
+    colnames(dt) <- c("tourn", "player", "year")
+    setorder(dt, tourn, player, year)
+
+    cat(":: prepared data.table with", nrow(dt), "rows and 3 columns. Bye!\n") 
+
+    return(list(all_data=data, tourn=all_tourn, players=all_names, dt=dt))
+}
+
+PrepareDataSackmann <- function(dir="SackmanGit/tennis_atp", pattern="atp_matches_[[:digit:]]{4}.csv") {
+
+    ## We match only the csv files that we want, e.g. no challenger here
+    lf <- list.files(path=dir, pattern=pattern, full.names=TRUE)
+    lf_nonfull <- list.files(path=dir, pattern=pattern, full.names=FALSE)
+    lf_nonfull <- sub("atp_matches_","",lf_nonfull)
+    lf_nonfull <- sub(".csv","",lf_nonfull)
+    
+    cat(paste(":: Found ", length(lf), "files, from", lf_nonfull[1], "to", lf_nonfull[length(lf_nonfull)], "\n")) 
+    
+    ## read the data
+    read_timing <- system.time(res <- lapply(lf, fread))
+    names(res) <- lf_nonfull
+    cat(paste(":: Reading done in", round(read_timing[3], 5), "s\n")) 
+    
+    ## flatten the list into a big data.table adding the column with the year
+    data <- rbindlist(res, use.names=TRUE, fill=FALSE, idcol=TRUE)
+    
+    ## exclude Davis cup matches and select only relevant columns
+    ind_davis <- grep("^Davis", data$tourney_name)
+    data <- data[-ind_davis, ] ###c(".id","tourney_date", "tourney_name","tourney_level", "draw_size", "round", "winner_name","loser_name")]
+
+    ## find out all tournaments
+    all_tourn <- sort(unique(data$tourney_name))
+    ## find out all players
+    all_names <- sort(unique(c(data$winner_name, data$loser_name)))
+
+    ## here .id column is the year
+    ## group all the finals per tournament, per winner and per year
+    nw <- data[,.N, by=.(tourney_name, winner_name, .id)]
+    ## this was faster but didn't work for round robin tournaments such as Tour Finals
+    ## nw <- data[round=="F",.N, by=.(tourney_name, winner_name, .id)]
+
+    ## find all losers per tournament and per year
+    nl <- data[,.N, by=.(tourney_name, loser_name, .id)]
+
+    ## perform the union of the two sets! Here, each row represents a player who played in that tournament in that year
+    dt <- rbindlist(list(nw, nl), use.names=FALSE)[, 1:3]
+    if (haskey(dt)) 
+        setkey(dt,NULL)
+    ## we need now to remove duplicates
+    dt <- unique(dt)
+    colnames(dt) <- c("tourn", "player", "year")
+    setorder(dt, tourn, player, year)
+
+    cat(":: prepared data.table with", nrow(dt), "rows and 3 columns. Bye!\n") 
+
+    return(list(all_data=data, tourn=all_tourn, players=all_names, dt=dt))
+}
+
+SearchByPlayer <- function(name, data, tour) {
+
+    if (!name %in% data$players)
+        stop(":: Missing player, check the spelling\n")
+
+    ## from the data
+    tab <- data$dt
+    ## first we look at the player
+    dtplayer <- tab[player==name, .N, by=.(tourn)][order(N, decreasing = TRUE)]
+    if (!missing(tour))
+        dtplayer <- dtplayer[tourn==tour]
+
+    return(dtplayer)
+}
