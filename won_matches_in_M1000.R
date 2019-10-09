61a0c718d13ee9a21d35641158569ab5afa38884
domanov77 <domanov@gmail.com>
2019-10-09 16:26:30 +0200

    fixes in matches_won_in_m1000 - excluding walkovers

--- won_matches_in_M1000.R
+++ won_matches_in_M1000.R
@@ -23,12 +23,21 @@ masters <- c("ATP Masters 1000 Canada",
 
 
 dbm <- db[tourney_name %in% masters,]
+
+### here including walkovers
 out <- dbm[,.N,by=winner_name]
 setorder(out, -N, na.last=FALSE)
 head(out)
+
+### here excluding walkovers
+out_nowo <- dbm[score!="W/O",.N,by=winner_name]
+setorder(out_nowo, -N, na.last=FALSE)
+head(out_nowo)
 
 aa <- dbm[round=="F", year, by=tourney_name]
 setorder(aa, year)
 
 OutputTableToPng(head(out,20), "MatchesWonInMasters.png")
-OutputTableToPng(aa, "ConsideredMasters.png")
+OutputTableToPng(head(out_nowo,20), "MatchesWonInMasters_noWO.png")
+OutputTableToPng(aa[seq(  1,134),], "ConsideredMasters_1.png")
+OutputTableToPng(aa[seq(135,268),], "ConsideredMasters_2.png")
